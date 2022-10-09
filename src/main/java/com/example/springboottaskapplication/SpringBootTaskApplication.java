d0bc2c989d538da1b459e587153351334271cbc7
Erik Yuzwa <erikyuzwa@gmail.com>
2022-10-08 18:29:12 -0600

    initial project generation

---
+++ src/main/java/com/example/springboottaskapplication/SpringBootTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottaskapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTaskApplication.class, args);
+	}
+
+}
